3ef8ba6ae7ec11a7f6cbf6b2a957006f6b164264
Eyal Haimov <eyalhaimoon@gmail.com>
2023-11-07 11:27:53 +0200

    update

--- KotlinBasics/app/src/main/java/eu/blindka/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/eu/blindka/kotlinbasics/Basics.kt
@@ -3,22 +3,20 @@ package eu.blindka.kotlinbasics
 fun main(){ // not a general function, but a starting point for our application, play '>' button for main function
     //val myName = "Eyal" // val is permanent and can't be overwritten, it runs much faster, use var when necessary only
     // TODO:  Add new functionality
-    var x = 100
-    while(x >= 0) {
-        if(x%2 == 0)
-        {
-            if(x==0)
-            {
-                println("$x.")
-                break
-            }
-            print("$x,")
-        }
-        x--
-    }
-    println("While loop is done!")
-    x = 1
     do{
+        println("$x")
         x++
-    }while (x<= 10)
+    }while (x<=10)
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while(feltTemp == "cold"){
+        roomTemp++
+        if(roomTemp >= 20){
+            feltTemp = "comfy"
+            println("It's comfy.")
+        }
+    }
+    for(num in 1..10){
+        println("$num")
+    }
 }
